74e0d4df4455d59fffb355f310fe74a7942bba49
Jonathon Zudell <jonathon.zudell@gmail.com>
2016-07-04 13:29:10 -0400

    Broke ghcn_granular apart itno tables only holding data for one element and only for WMO stations

--- scripts/ingest/format_data.py
+++ scripts/ingest/format_data.py
@@ -4,9 +4,11 @@ import os
 import time
 import gzip
 import sys
+import subprocess
+import cStringIO
 
 #POTENTIALLY OPTIMIZE
-def format_by_key(line, accepted_elements):
+def format_by_key(line, accepted_elements, wmo_stations):
     variables = line.split(',')
     result = {}
     result['ID']      = variables[0]
@@ -26,15 +28,14 @@ def format_by_key(line, accepted_elements):
     else:
         result['HOUR']    = ''
         result['MINUTE']  = ''
-
     
-    if result['ELEMENT'] in accepted_elements:
+    if result['ELEMENT'] in accepted_elements and result['ID'] in wmo_stations:
         return result
     else:
         return None
 
 def create_csv_line(line):
-    ordered_keys = ['ID','YEAR','MONTH','DAY','HOUR','MINUTE','ELEMENT','VALUE','M_FLAG','Q_FLAG','S_FLAG']
+    ordered_keys = ['ID','YEAR','MONTH','DAY','HOUR','MINUTE','VALUE','M_FLAG','Q_FLAG','S_FLAG']
     result = ''
     for key in ordered_keys:
         result += str(line[key])
@@ -43,53 +44,82 @@ def create_csv_line(line):
         else:
             result += ','
     return result
+
+def get_wmo_stations(cursor):
+    cursor.execute('''SELECT "ID" FROM "META"."STATIONS" WHERE "WMO_ID" <> '' ''')
+    result = cursor.fetchall()
+    result = [ item[0] for item in result ]
+    return set(result)
     
     
-def get_accepted_elements():
-    conn = psycopg2.connect("host='{0}' dbname='{1}' user='{2}' password='{3}'".format(os.environ['INGEST_HOST'],
-                                                                                       os.environ['INGEST_DB'],
-                                                                                       os.environ['INGEST_USER'],
-                                                                                       os.environ['INGEST_PASS']))
-    cursor = conn.cursor()
+def get_accepted_elements(cursor):
     cursor.execute('SELECT "ELEMENT" FROM "META"."ELEMENTS_DEFINITION"')
     result = cursor.fetchall()
     result = [ item[0] for item in result ]
-    conn.close()
-    return result
+    return set(result)
 
-#BATCH WRITE FORMATTED LINES
-def format(given_file, target_file, accepted_elements):
-    result = []
+def yield_lines(given_file):
+    sub_process = subprocess.Popen(["zcat", given_file], stdout = subprocess.PIPE)
+    result_file = cStringIO.StringIO(sub_process.communicate()[0])
+    for line in [line.rstrip('\n') for line in result_file]:
+        yield line
     
-    with gzip.open(given_file, 'r') as this_file, open(target_file, 'w+') as output_file:
-        for line in this_file.readlines():
-            formatted_line = format_by_key(line, accepted_elements)
-            if formatted_line is not None:
-                result.append(create_csv_line(formatted_line))
-                if len(result) > 9999:
-                    output_file.write(''.join(result))
-                    result = []
-        if len(result) > 0:
-            output_file.write(''.join(result))
-    os.remove(given_file)
+#BATCH WRITE FORMATTED LINES
+def format_files(given_file, target_file, accepted_elements, wmo_stations):
+    lists_by_element = {}
 
-def run(start,end):
+    for line in yield_lines(given_file):
+        formatted_line = format_by_key(line, accepted_elements, wmo_stations)
+        if formatted_line is not None:
+            if formatted_line['ELEMENT'] not in lists_by_element.keys():
+                lists_by_element[formatted_line['ELEMENT']] = []
+            lists_by_element[formatted_line['ELEMENT']].append(create_csv_line(formatted_line))
+    
+    for key in lists_by_element.keys():
+        divided_target = target_file.split('.')[0] + '_' + key + '.' + target_file.split('.')[1]
+        with open(divided_target, 'w+') as output_file:
+            for line in lists_by_element[key]:
+                output_file.write(line)
+            
+            
+    #with open(target_file, 'w+') as output_file:
+        #for line in yield_lines(given_file):
+            #formatted_line = format_by_key(line, accepted_elements, wmo_stations)
+            #if formatted_line is not None:
+                #result.append(create_csv_line(formatted_line))
+                #if len(result) > 9999:
+                    #output_file.write(''.join(result))
+                    #result = []
+        #if len(result) > 0:
+            #output_file.write(''.join(result))
+    #os.remove(given_file)
+
+def run(start, end, conn):
     input_dir = os.environ['DATA_DROP'] + '/yearly/unformatted/'
     target = os.environ['DATA_DROP'] + '/yearly/formatted/'
     files = [ f for f in os.listdir(input_dir) if start < int(f.split('.')[0]) <= end ]
-    accepted_elements = get_accepted_elements()
+
+    cursor = conn.cursor()
+    accepted_elements = get_accepted_elements(cursor)
+    wmo_stations = get_wmo_stations(cursor)
+
     print 'BEGINNING FORMAT GHCN DATA PROCEDURE'
     total = 0
     for item in files:
         print 'STARTING FORMAT FOR {0}'.format(item)
         start = time.time()
-        format(input_dir + item, target + '.'.join(item.split('.')[0:2]), accepted_elements)
+        format_files(input_dir + item, target + '.'.join(item.split('.')[0:2]), accepted_elements, wmo_stations)
         end = time.time() - start
         total += time.time() - start
         print 'ENDING FORMAT FOR {0} COMPLETED IN {1} TIME'.format(item, end)
     print 'ALL FORMATS FINISHED IN {0} TIME'.format(total)
 	
 if __name__ == '__main__':
+    conn = psycopg2.connect("host='{0}' dbname='{1}' user='{2}' password='{3}'".format(os.environ['INGEST_HOST'],
+                                                                                       os.environ['INGEST_DB'],
+                                                                                       os.environ['INGEST_USER'],
+                                                                                       os.environ['INGEST_PASS']))
     start = int(sys.argv[1])
     end = int(sys.argv[2])
-    run(start,end)
+    run(start,end, conn)
+    conn.close()
--- scripts/ingest/load_data.py
+++ scripts/ingest/load_data.py
@@ -4,35 +4,40 @@ import os
 import time
 import sys
 
-def load_file(loadable, cur, cols):
+def load_file(loadable, file_type, cur, cols):
     this_file = open(loadable,'r')
-
-    cur.copy_from(this_file, '"GHCN_DATA"."GHCN_GRANULAR"', sep=',', null='', columns=cols)
+    cur.copy_from(this_file, '"GHCN_DATA"."GHCN_{0}"'.format(file_type), sep=',', null='', columns=cols)
 
 def run(start,end,conn):
     files_dir = os.environ['DATA_DROP'] + '/yearly/formatted/'
-    files = [ f for f in os.listdir(files_dir) if start < int(f.split('.')[0]) <= end ]
 
-    cols= ('"ID"','"YEAR"','"MONTH"','"DAY"','"HOUR"','"MINUTE"','"ELEMENT"','"VALUE"','"M_FLAG"','"Q_FLAG"','"S_FLAG"')
+    cols= ('"ID"','"YEAR"','"MONTH"','"DAY"','"HOUR"','"MINUTE"','"VALUE"','"M_FLAG"','"Q_FLAG"','"S_FLAG"')
     total = 0
     cur = conn.cursor()
     print 'BEGINNING LOAD GHCN DATA PROCEDURE'
+    file_types = ['PRCP', 'SNOW','SNWD', 'TMAX', 'TAVG', 'TMIN', 'AWND', 'AWDR']
+    files = [ f for f in os.listdir(files_dir) if start < int(f.split('.')[0].split('_')[0]) <= end]
     for loadable in files:
         print 'LOADING {0}'.format(loadable)
         start = time.time()
-        load_file(files_dir + loadable,cur, cols)
+        file_type = None
+        for f_type in file_types:
+            if f_type in loadable:
+                file_type = f_type
+        load_file(files_dir + loadable, file_type,cur, cols)
         end = time.time() - start
         total += end
-        os.remove(files_dir + loadable)
+        #os.remove(files_dir + loadable)
         print 'FINISHED LOAD FOR {0} in {1}'.format(loadable, end)
         conn.commit()
     cur.close()
     print 'FINISHED LOAD FOR {0} FILES in {1}'.format(len(files), total)
+
 if __name__ == '__main__':
     start = int(sys.argv[1])
     end = int(sys.argv[2])
     conn = psycopg2.connect("host='{0}' dbname='{1}' user='{2}' password='{3}'".format(os.environ['INGEST_HOST'],
-	                                                                                   os.environ['INGEST_DB'],
-																					   os.environ['INGEST_USER'],
-																					   os.environ['INGEST_PASS']))
+	                                                                               os.environ['INGEST_DB'],
+										       os.environ['INGEST_USER'],
+										       os.environ['INGEST_PASS']))
     run(start,end,conn)
